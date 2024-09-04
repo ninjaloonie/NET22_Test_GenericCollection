5f3e0836d5b2b5e40eb62e990a706f87ffffdc52
Brian Angyang <n01713422@humber.ca>
2024-09-03 20:32:09 -0400

    Add project files.

---
+++ Runner.cs
@@ -0,0 +1,22 @@
+﻿using FinalExam;
+
+namespace GenericCollectionItem
+{
+    public class Runner
+    {
+        public static void Main(string[] args)
+        {
+            TestCollection<int> items = new TestCollection<int>();
+            Random random = new Random();
+            for (int i = 1; i < 10; i++)
+            {
+                items.Add(random.Next(99));
+            }
+
+            foreach(int i in items.ListDescending())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ TestCollection.cs
@@ -0,0 +1,30 @@
+﻿namespace FinalExam
+{
+    public class TestCollection<T> where T : struct
+    {
+        private readonly List<T> _items;
+
+        public TestCollection()
+        {
+            _items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Get(int index)
+        {
+            return _items[index];
+        }
+
+        public List<T> ListDescending()
+        {
+            var items = new List<T>(_items);
+            items.Sort();
+            items.Reverse();
+            return items;
+        }
+    }
+}
